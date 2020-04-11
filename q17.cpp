c61d99437e7e5716f4c52c76dd9d83224f0294d5
Kanchon420 <63488350+Kanchon420@users.noreply.github.com>
2020-04-11 17:49:58 +0530

    Add files via upload

---
+++ q17.cpp
@@ -0,0 +1,84 @@
+/*. Design a scheduling program to implements a Queue with two levels:
+Level 1 : Fixed priority preemptive Scheduling
+Level 2: Round Robin Scheduling
+For a Fixed priority preemptive Scheduling (Queue1), the Priority 0 is highest priority. If one
+process P1 is scheduled and running, another process P2 with higher priority comes. The New
+process (high priority) process P2 preempts currently running process P1 and process P1 will go
+to second level queue. Time for which process will strictly execute must be considered in the
+multiples of 2. All the processes in second level queue will complete their execution according to
+round robin scheduling.
+Consider: 1. Queue 2 will be processed after Queue 1 becomes empty.
+2. Priority of Queue 2 has lower priority than in Queue 1*/
+
+#include<stdio.h>
+#include<conio.h>
+ 
+int main()
+{
+    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+    printf("Enter Total Number of Process:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Burst Time and Priority\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\nP[%d]\n",i+1);
+        printf("Burst Time:");
+        scanf("%d",&bt[i]);
+        printf("Priority:");
+        scanf("%d",&pr[i]);
+        p[i]=i+1;           
+    }
+ 
+    
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+ 
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0;
+ 
+    
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avg_wt=total/n;   
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i]; 
+        total+=tat[i];
+        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+ 
+    avg_tat=total/n;  
+    printf("\n\nAverage Waiting Time=%d",avg_wt);
+    printf("\nAverage Turnaround Time=%d\n",avg_tat);
+ 
+	return 0;
+}
